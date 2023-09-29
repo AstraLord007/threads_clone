63e355361a07a15efd43c899274be6f45e2b1bde
Mauricio Dorado Lenz <mauridlenz@gmail.com>
2023-09-29 14:44:50 -0300

    Form components added.

--- components/forms/PostThread.tsx
+++ components/forms/PostThread.tsx
@@ -11,7 +11,7 @@ import { usePathname, useRouter } from "next/navigation";
 import { ThreadValidation } from "@/lib/validations/thread";
 
 import {
-    Form,
+	Form,
 	FormControl,
 	FormField,
 	FormItem,
@@ -34,17 +34,43 @@ interface Props {
 
 
 function PostThread({ userId }: { userId: string }) {
-    const router = useRouter();
-  	const pathname = usePathname();
+	const router = useRouter();
+	const pathname = usePathname();
 
 	const form = useForm({
 		resolver: zodResolver(ThreadValidation),
 		defaultValues: {
 			thread: '',
-            accounId: userId,
+			accounId: userId,
 		},
 	});
-    return <h1>Post Thread Form</h1>
+	return (
+		<Form {...form}>
+			<form
+				className='flex flex-col justify-start gap-10'
+				onSubmit={form.handleSubmit(onSubmit)}
+			></form>
+
+			<FormField
+				control={form.control}
+				name='thread'
+				render={({ field }) => (
+					<FormItem className='flex w-full flex-col gap-3'>
+						<FormLabel className='text-base-semibold text-light-2'>
+							Content
+						</FormLabel>
+						<FormControl className="no-focus border border-dark-4 bg-dark-3 text-light-1">
+							<Textarea
+								rows={15}
+								{...field}
+							/>
+						</FormControl>
+						<FormMessage />
+					</FormItem>
+				)}
+			/>
+		</Form>
+	)
 }
 
 export default PostThread;
